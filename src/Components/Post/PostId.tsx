900a5c23e71830f6699cc9f5f72b44fa6f09b165
ppadti <ppadti@redhat.com>
2022-10-06 21:29:42 +0530

    Add layout to the drwaer of PostId component

--- src/Components/Post/PostId.tsx
+++ src/Components/Post/PostId.tsx
@@ -9,35 +9,34 @@ import {
   Text,
   TextVariants,
   Avatar,
-  Flex,
-  FlexItem,
-  CardHeader,
   TextArea,
-  Panel,
-  PanelMainBody,
-  PanelMain,
   Form,
   FormGroup,
   TextInput,
+  Truncate,
+  Drawer,
+  DrawerPanelContent,
+  DrawerContent,
+  DrawerHead,
+  DrawerActions,
+  DrawerCloseButton,
+  Stack,
+  StackItem,
+  DrawerPanelBody,
+  Flex,
+  FlexItem,
+  PageSectionVariants,
 } from '@patternfly/react-core'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
-import {
-  Drawer,
-  DrawerPanelContent,
-  DrawerContent,
-  DrawerContentBody,
-  DrawerHead,
-  DrawerActions,
-  DrawerCloseButton,
-} from '@patternfly/react-core'
+
 const avatarImg =
   'https://www.patternfly.org/v4/images/avatarImg.668560cdf25a4932ef9f711b4acad52d.svg'
 
 const PostId = () => {
   const location = useLocation()
-  const id: any = location.state
+  const postId: any = location.state
   const [postData, setPostData] = useState<{ [key: string]: any }>({})
   const [commentData, setCommentData] = useState<
     { postId: number; id: number; name: string; email: string; body: string }[]
@@ -48,9 +47,11 @@ const PostId = () => {
   const drawerRef = React.useRef<HTMLDivElement>()
   const [showing, setShowing] = useState(false)
   const [comment, setComment] = useState('')
+  const [name, setName] = React.useState('')
+  const [email, setEmail] = React.useState('')
 
   useEffect(() => {
-    axios(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    axios(`https://jsonplaceholder.typicode.com/posts/${postId}`)
       .then((response) => {
         setPostData(response.data)
       })
@@ -59,17 +60,12 @@ const PostId = () => {
         console.error('Error in loading data', error)
         setPostError(error)
       })
-  }, [id])
+  }, [])
 
   useEffect(() => {
-    axios('https://jsonplaceholder.typicode.com/comments')
+    axios(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
       .then((response) => {
-        setCommentData(
-          response.data?.filter(
-            (comment: { postId: any }) => comment.postId === id,
-          ),
-        )
-        console.log(commentData.length)
+        setCommentData(response.data)
       })
 
       .catch((error) => {
@@ -79,7 +75,6 @@ const PostId = () => {
   }, [])
 
   const count = commentData.length
-  console.log(commentData)
 
   const onExpand = () => {
     drawerRef.current && drawerRef.current.focus()
@@ -108,20 +103,15 @@ const PostId = () => {
       setCommentData((prevState) => [
         ...prevState,
         {
-          postId: id,
-          id: id,
+          postId,
+          id: postId,
           name: name,
           email: email,
           body: comment,
         },
       ])
-
-      console.log(commentData.length)
     }
   }
-
-  const [name, setName] = React.useState('')
-  const [email, setEmail] = React.useState('')
 
   const handleNameChange = (name: string) => {
     setName(name)
@@ -137,139 +127,153 @@ const PostId = () => {
 
   const panelContent = (
     <DrawerPanelContent>
-      <Panel isScrollable>
-        <PanelMain maxHeight="70rem">
-          <DrawerHead>
-            <span tabIndex={isExpanded ? 0 : -1}>
-              {error ? (
-                <TextContent>Something went wrong...</TextContent>
-              ) : (
-                <>
-                  <TextContent>
-                    <Text component={TextVariants.h1}>Comments ({count})</Text>
-                  </TextContent>
-
-                  {/* <hr></hr> */}
-                  {commentData.map((comment) => (
-                    <Card key={comment['id']}>
-                      <CardHeader>
-                        <CardTitle>
-                          <Avatar src={avatarImg} alt="avatar" />
-                        </CardTitle>
-                        <CardTitle style={{ paddingLeft: '0.5rem' }}>
+      <DrawerHead>
+        <Stack>
+          <StackItem>
+            <TextContent>
+              <Text component="h1">Comments ({count})</Text>
+            </TextContent>
+          </StackItem>
+        </Stack>
+        <DrawerActions>
+          <DrawerCloseButton onClick={onCloseClick} />
+        </DrawerActions>
+      </DrawerHead>
+      <DrawerPanelBody>
+        {error ? (
+          <TextContent>
+            {' '}
+            <Text component={TextVariants.h2}>Unable to load...</Text>
+          </TextContent>
+        ) : (
+          <>
+            {commentData.map((comment) => (
+              <>
+                <Card style={{ margin: '1rem' }}>
+                  <CardTitle>
+                    <Flex style={{ alignItems: 'center' }}>
+                      <Flex direction={{ default: 'column' }}>
+                        <FlexItem>
+                          <Avatar
+                            src={avatarImg}
+                            alt="avatar"
+                            size="md"
+                            border="dark"
+                          />
+                        </FlexItem>
+                      </Flex>
+                      <Flex
+                        direction={{ default: 'column' }}
+                        flex={{ default: 'flex_1' }}
+                      >
+                        <FlexItem style={{ margin: '0rem' }}>
                           <Text component={TextVariants.h2}>
-                            {comment['name']}
+                            <Truncate content={comment.name}></Truncate>
                           </Text>
-                        </CardTitle>
-                      </CardHeader>
-                      <CardBody>{comment['email']}</CardBody>
-                      <CardBody>{comment['body']}</CardBody>
-                    </Card>
-                  ))}
-                  <br></br>
-                  <Button variant="primary" onClick={handleCommentSection}>
-                    Add Comment
-                  </Button>
-                  {showing && (
-                    <>
-                      <Form>
-                        <FormGroup
-                          label="Full name"
-                          isRequired
-                          fieldId="simple-form-name-01"
-                        >
-                          <TextInput
-                            isRequired
-                            type="text"
-                            id="simple-form-name-01"
-                            name="simple-form-name-01"
-                            aria-describedby="simple-form-name-01-helper"
-                            value={name}
-                            onChange={handleNameChange}
-                          />
-                        </FormGroup>
-                        <FormGroup
-                          label="Email"
-                          isRequired
-                          fieldId="simple-form-email-01"
-                        >
-                          <TextInput
-                            isRequired
-                            type="email"
-                            id="simple-form-email-01"
-                            name="simple-form-email-01"
-                            value={email}
-                            onChange={handleEmailChange}
-                          />
-                        </FormGroup>
-                        <FormGroup
-                          label="Your comments"
-                          isRequired
-                          fieldId="horizontal-form-exp"
-                        >
-                          <TextArea
-                            placeholder="Enter your comment"
-                            value={comment}
-                            name="inputGroup-with-textarea"
-                            id="inputGroup-with-textarea"
-                            aria-label="textarea with button"
-                            onChange={updateComment}
-                          ></TextArea>
-                        </FormGroup>
-                        <ActionGroup>
-                          <Button
-                            id="textAreaButton2"
-                            variant="primary"
-                            onClick={addComment}
-                          >
-                            Add
+                        </FlexItem>
+                        <FlexItem>
+                          <Button isInline component="span" variant="link">
+                            {comment.email}
                           </Button>
-                        </ActionGroup>
-                      </Form>
-                    </>
-                  )}
-                </>
-              )}
-            </span>
-            <DrawerActions>
-              <DrawerCloseButton onClick={onCloseClick} />
-            </DrawerActions>
-          </DrawerHead>
-        </PanelMain>
-      </Panel>
+                        </FlexItem>
+                      </Flex>
+                    </Flex>
+                  </CardTitle>
+                  <CardBody>{comment.body}</CardBody>
+                </Card>
+              </>
+            ))}
+            <Button variant="primary" onClick={handleCommentSection}>
+              Add Comment
+            </Button>
+            {showing && (
+              <Form>
+                <FormGroup
+                  label="Full name"
+                  isRequired
+                  fieldId="simple-form-name-01"
+                >
+                  <TextInput
+                    isRequired
+                    type="text"
+                    id="simple-form-name-01"
+                    name="simple-form-name-01"
+                    aria-describedby="simple-form-name-01-helper"
+                    value={name}
+                    onChange={handleNameChange}
+                  />
+                </FormGroup>
+                <FormGroup
+                  label="Email"
+                  isRequired
+                  fieldId="simple-form-email-01"
+                >
+                  <TextInput
+                    isRequired
+                    type="email"
+                    id="simple-form-email-01"
+                    name="simple-form-email-01"
+                    value={email}
+                    onChange={handleEmailChange}
+                  />
+                </FormGroup>
+                <FormGroup
+                  label="Your comments"
+                  isRequired
+                  fieldId="horizontal-form-exp"
+                >
+                  <TextArea
+                    placeholder="Enter your comment"
+                    value={comment}
+                    name="inputGroup-with-textarea"
+                    id="inputGroup-with-textarea"
+                    aria-label="textarea with button"
+                    onChange={updateComment}
+                  ></TextArea>
+                </FormGroup>
+                <ActionGroup>
+                  <Button variant="primary" onClick={addComment}>
+                    Add
+                  </Button>
+                </ActionGroup>
+              </Form>
+            )}
+          </>
+        )}
+      </DrawerPanelBody>
     </DrawerPanelContent>
   )
 
+  const pageContent = (
+    <>
+      {postError ? (
+        <TextContent>Something went wrong...</TextContent>
+      ) : (
+        <>
+          <PageSection variant={PageSectionVariants.light}>
+            <Card style={{ marginBottom: '1rem' }}>
+              <CardTitle>Post - {postData.id}</CardTitle>
+              <CardTitle>{postData.title}</CardTitle>
+              <CardBody>{postData.body}</CardBody>
+            </Card>
+
+            <ActionGroup>
+              <Button
+                variant="secondary"
+                aria-expanded={isExpanded}
+                onClick={onClick}
+              >
+                Read Comments ({count})
+              </Button>
+            </ActionGroup>
+          </PageSection>
+        </>
+      )}
+    </>
+  )
   return (
     <Drawer isExpanded={isExpanded} onExpand={onExpand}>
-      <DrawerContent panelContent={panelContent}>
-        <DrawerContentBody>
-          {postError ? (
-            <TextContent>Something went wrong...</TextContent>
-          ) : (
-            <>
-              <PageSection>
-                <Card>
-                  <CardTitle>Post - {postData.id}</CardTitle>
-                  <CardTitle>{postData.title}</CardTitle>
-                  <CardBody>{postData.body}</CardBody>
-                </Card>
-                <br></br>
-
-                <ActionGroup>
-                  <Button
-                    variant="secondary"
-                    aria-expanded={isExpanded}
-                    onClick={onClick}
-                  >
-                    Read Comments ({count})
-                  </Button>
-                </ActionGroup>
-              </PageSection>
-            </>
-          )}
-        </DrawerContentBody>
-      </DrawerContent>
+      <DrawerContent panelContent={panelContent}>{pageContent}</DrawerContent>
     </Drawer>
   )
 }
